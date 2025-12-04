9b9a497e99a777be75b77db457f9f892eb54189c
jyonio <jyonio9191@gmail.com>
2025-12-04 21:40:38 +0900

    first commit

---
+++ src/main/java/com/example/evolon/EvolonApplication.java
@@ -0,0 +1,13 @@
+package com.example.evolon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evolon/EvolonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evolon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
